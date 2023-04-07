3d7390d98366956e49d699132784820591d31941
gpsign <gpsignorini@gmail.com>
2023-04-07 03:45:10 -0300

    add verificacao de par

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 const mesa = document.querySelector(".mesa");
 let nmrCartas = 0;
 let deck = [];
+let buffer = [];
 let pares = [
   "bobrossparrot",
   "explodyparrot",
@@ -10,6 +11,7 @@ let pares = [
   "tripletsparrot",
   "unicornparrot",
 ];
+
 pares.sort(comparador);
 
 while (nmrCartas < 4 || nmrCartas > 14 || nmrCartas % 2 !== 0) {
@@ -37,20 +39,49 @@ for (let i = 0; i < nmrCartas / 2; i++) {
 
 deck.sort(comparador);
 
-mesa.innerHTML = deck;
+mesa.innerHTML = deck.join(" ");
 
-function comparador() { 
-	return Math.random() - 0.5; 
+function comparador() {
+  return Math.random() - 0.5;
 }
 
-function clicou(carta){
-
- carta.firstElementChild.style.transform = "rotateY(-180deg)";
- carta.lastElementChild.style.transform = "rotateY(0deg)";
- setTimeout(() => desclicar(carta), 3000);
+function clicou(carta) {
+  if (buffer.length < 2 && !carta.classList.contains("virada")) {
+    carta.firstElementChild.style.transform = "rotateY(-180deg)";
+    carta.lastElementChild.style.transform = "rotateY(0deg)";
+    buffer.push(carta);
+    carta.classList.add("virada");
+    if (!verificarCarta()) setTimeout(desvirar, 1000);
+  }
 }
 
-function desclicar(carta){
-  carta.firstElementChild.style.transform = "rotateY(0deg)";
- carta.lastElementChild.style.transform = "rotateY(-180deg)";
+function verificarCarta() {
+  if (buffer.length === 2) {
+    console.log(buffer[0].id);
+    if (
+      buffer[0].lastElementChild.firstElementChild.src ==
+      buffer[1].lastElementChild.firstElementChild.src
+    ) {
+      buffer = [];
+      console.log("sim");
+      return true;
+    } else {
+      console.log("nao");
+      return false;
+    }
+  } else return true;
+}
+
+function desvirar() {
+  buffer[0].firstElementChild.style.transform = "rotateY(0deg)";
+  buffer[0].lastElementChild.style.transform = "rotateY(-180deg)";
+  
+  buffer[1].firstElementChild.style.transform = "rotateY(0deg)";
+  buffer[1].lastElementChild.style.transform = "rotateY(-180deg)";
+
+  setTimeout(() => {
+    buffer[0].classList.remove("virada");
+    buffer[1].classList.remove("virada");
+    buffer = [];
+  }, 500);
 }
