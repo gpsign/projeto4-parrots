3a21bced359dafbac7d4893acc6fc2735a48db6e
gpsign <gpsignorini@gmail.com>
2023-04-07 17:51:13 -0300

    Finalizado SEM DATA-TEST

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,9 @@
 const mesa = document.querySelector(".mesa");
 let nmrCartas = 0;
+let jogadas = 0;
+let paresFeitos = 0;
+let tempo = 0;
+let funcaoTempo;
 let deck = [];
 let buffer = [];
 let pares = [
@@ -11,35 +15,51 @@ let pares = [
   "tripletsparrot",
   "unicornparrot",
 ];
-
 pares.sort(comparador);
 
-while (nmrCartas < 4 || nmrCartas > 14 || nmrCartas % 2 !== 0) {
-  nmrCartas = prompt("Numero de cartas:");
-}
+comecarJogo();
 
-for (let i = 0; i < nmrCartas / 2; i++) {
-  deck.push(`<div class="card" onclick="clicou(this)">
+function comecarJogo() {
+  nmrCartas = 0;
+  jogadas = 0;
+  paresFeitos = 0;
+  tempo = 0;
+  deck = [];
+
+  while (nmrCartas < 4 || nmrCartas > 14 || nmrCartas % 2 !== 0) {
+    nmrCartas = prompt("Numero de cartas:");
+  }
+
+  for (let i = 0; i < nmrCartas / 2; i++) {
+    deck.push(`<div class="card" onclick="clicou(this)">
+    <div class="front-face face">
+      <img src="./imgs/back.png"></img>
+    </div>
+    <div class="back-face face">
+      <img src="./imgs/${pares[i]}.gif"></img>
+    </div>
+  </div>`);
+    deck.push(`<div class="card" onclick="clicou(this)">
   <div class="front-face face">
     <img src="./imgs/back.png"></img>
   </div>
   <div class="back-face face">
     <img src="./imgs/${pares[i]}.gif"></img>
   </div>
-</div>`);
-  deck.push(`<div class="card" onclick="clicou(this)">
-<div class="front-face face">
-  <img src="./imgs/back.png"></img>
-</div>
-<div class="back-face face">
-  <img src="./imgs/${pares[i]}.gif"></img>
-</div>
-</div>`);
+  </div>`);
+  }
+
+  deck.sort(comparador);
+
+  mesa.innerHTML = deck.join(" ");
+  mesa.style.gridTemplateColumns = `repeat(${deck.length / 2}, 117px)`;
+
+  document.querySelector(".cronometro").innerHTML = `<p>0</p>`;
+  funcaoTempo = setInterval(() => {
+    tempo++;
+    document.querySelector(".cronometro").innerHTML = `<p>${tempo}</p>`;
+  }, 1000);
 }
-
-deck.sort(comparador);
-
-mesa.innerHTML = deck.join(" ");
 
 function comparador() {
   return Math.random() - 0.5;
@@ -47,6 +67,7 @@ function comparador() {
 
 function clicou(carta) {
   if (buffer.length < 2 && !carta.classList.contains("virada")) {
+    jogadas++;
     carta.firstElementChild.style.transform = "rotateY(-180deg)";
     carta.lastElementChild.style.transform = "rotateY(0deg)";
     buffer.push(carta);
@@ -63,10 +84,24 @@ function verificarCarta() {
       buffer[1].lastElementChild.firstElementChild.src
     ) {
       buffer = [];
-      console.log("sim");
+      paresFeitos++;
+      if (paresFeitos === nmrCartas / 2) {
+        buffer = [];
+        clearInterval(funcaoTempo);
+        alert(
+          `Você ganhou em ${jogadas} jogadas! A duração do jogo foi de ${tempo} segundos!`
+        );
+        let reiniciar = "";
+        while (true) {
+          reiniciar = prompt("Começar um novo jogo?");
+          if (reiniciar === "sim") {
+            comecarJogo();
+            break;
+          } else if (reiniciar === "não") break;
+        }
+      }
       return true;
     } else {
-      console.log("nao");
       return false;
     }
   } else return true;
@@ -75,7 +110,7 @@ function verificarCarta() {
 function desvirar() {
   buffer[0].firstElementChild.style.transform = "rotateY(0deg)";
   buffer[0].lastElementChild.style.transform = "rotateY(-180deg)";
-  
+
   buffer[1].firstElementChild.style.transform = "rotateY(0deg)";
   buffer[1].lastElementChild.style.transform = "rotateY(-180deg)";
 
